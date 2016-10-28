1b8cfdb4746f13332b1a8902cce1ec402533b7e1
Maartje Eyskens <meyskens@me.com>
2016-10-28 13:19:00 +0200

    patch day issues

--- components/quivr/api.js
+++ components/quivr/api.js
@@ -6,10 +6,18 @@ export const login = (email, password) => new Promise((resolve) => {
 
 export const getSchedule = (authToken) => new Promise((resolve) => {
     const now = new Date()
+    let nextMonth = ("0" + (now.getMonth() + 2)).slice(-2)
+    let nextDay = ("0" + now.getDate()).slice(-2)
+    if (parseInt(nextMonth, 10) > 12) {
+        nextMonth = "01"
+    }
+    if (parseInt(nextDay, 10) > 30) {
+        nextDay = "30"
+    }
     rest.postJson("https://quivr.be/v3/ScheduleItem/list", {
         "userIDList": [],
         "startDate": `${now.getFullYear()}-${("0" + (now.getMonth() + 1)).slice(-2)}-${("0" + now.getDate()).slice(-2)}T00:00:00+02:00`,
-        "endDate": `${now.getFullYear()}-${("0" + (now.getMonth() + 2)).slice(-2)}-${("0" + now.getDate()).slice(-2)}T00:00:00+02:00`,
+        "endDate": `${now.getFullYear()}-${nextMonth}-${nextDay}T00:00:00+02:00`,
     }, {
         headers: {
             "Accept": "application/json",
